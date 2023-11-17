9338b86781fb6e07d947e17eb16de18ea3a8e6a3
Abel <huanengchen@foxmail.com>
2023-11-17 17:38:43 +0800

    Finished the HW1.

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -58,6 +58,9 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
      */
     @Override
     public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
         return rb[first];
     }
 
